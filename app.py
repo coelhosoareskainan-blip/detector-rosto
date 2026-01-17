3bda489930e89ba883e2d7823843a1f71e4e3789
coelhosoareskainan-blip <coelhosoareskainan@gmail.com>
2026-01-17 13:44:30 -0300

    app.py

--- app.py
+++ app.py
@@ -14,7 +14,11 @@ uploaded_file = st.file_uploader("📷 Envie uma imagem", type=["jpg", "jpeg", "
 if uploaded_file:
     image = Image.open(uploaded_file)
     img_array = np.array(image)
-    img_rgb = cv2.cvtColor(img_array, cv2.COLOR_BGR2RGB)
+    if img_array.shape[2] == 4:
+    img_array = cv2.cvtColor(img_array, cv2.COLOR_BGRA2BGR)
+
+img_rgb = cv2.cvtColor(img_array, cv2.COLOR_BGR2RGB)
+
 
     mp_face = mp.solutions.face_detection
     face_detection = mp_face.FaceDetection(model_selection=0, min_detection_confidence=0.6)
